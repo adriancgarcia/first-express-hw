16c4d636da9078e170145db0a48bb5238f146ba7
adriancgarcia <adriancgarcia@gmail.com>
2023-06-12 23:33:17 -0500

    Magic 8 Ball express app created

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 // require("dotenv").config() 
 const express = require("express")
-// const morgan = require("morgan") // importing the morgan library
 // const PORT = process.env.PORT
 const app = express();
-
-// app.use(morgan("dev"))
-
 
 
 app.get("/greeting/Kenzo/", (req, res) => {
@@ -13,12 +9,22 @@ app.get("/greeting/Kenzo/", (req, res) => {
 })
 
 app.get("/tip/:total/:tipPercentage/", (req, res) => {
-    // const tipPercentage = (x * .01)
     const tipAmount = (req.params.total / 100) * req.params.tipPercentage;
         console.log(tipAmount)
         res.send("the tip amount is " + tipAmount)
-
 })
+
+// app.get("/magic", (req, res) => {
+    const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+    app.get("/magic", (req, res) => {
+        for (let i = 0; i < answers[i].length; i ++) {
+            res.send(`<h1> ${answers[Math.floor(Math.random() * answers.length)]} </h1>`)
+            
+       }
+
+        })
+
 
 
 
@@ -26,3 +32,4 @@ app.get("/tip/:total/:tipPercentage/", (req, res) => {
 app.listen(3000, () => {
     console.log(`Listening on port 3000`)
 })
+// })
